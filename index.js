380d89b1d0c5cd6720c4e5ab88636bd34776752b
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-11-29 01:57:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+
+    let array = []
+    for(let i = 0; i < names.length; i++){
+        array[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return array;
+}
+
+
+function countDown(number){
+    let i = 0;
+    
+    while (number >= i){
+        console.log(number--);
+    }
+}
