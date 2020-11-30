5b18d7d00b163a729c480239a15b093573c1db8e
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-11-30 01:27:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(string_names, event_name) {
+    let new_array = [];
+    for (let i = 0; i < string_names.length; i++) {
+        new_array.push(`Thank you, ${string_names[i]}, for the wonderful ${event_name} gift!`)
+    }
+    return new_array 
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--);
+    } 
+}
