79bb27a3a693ee9af9f443e716c5abf029a40757
angela stephens <angelastephens317@gmail.com>
2020-12-03 19:34:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+function writeCards(names) {
+    let birthday = [];
+    for (let i = 0; i < names.length; i++) {
+        birthday[i] = 'Thank you, ' + names[i] + ', for the wonderful surprise gift!';
+    }
+    return birthday;
+}
+
+let number = 10;
+function countDown (number){
+    while (number >= 0 ){
+        console.log(number--);
+    }
+}
