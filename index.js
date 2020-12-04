98976dae64dbb9241fe5ae93783197f9673b500d
iamcid <iamcid17@gmail.com>
2020-12-04 00:40:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let cards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(startingInteger) {
+    while (startingInteger > 0) {
+        console.log(startingInteger);
+        startingInteger -= 1;
+    }
+    console.log(startingInteger);
+}
