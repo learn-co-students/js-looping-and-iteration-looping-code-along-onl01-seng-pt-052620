1e12011c3f201a35685f3954445a64899a15cbea
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 21:28:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameArray, celebration) {
+  let thankYouCards = [];
+  for(let name = 0; name < nameArray.length; name++) {
+    thankYouCards.push(`Thank you, ${nameArray[name]}, for the wonderful ${celebration} gift!`);
+  }
+  return thankYouCards;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
